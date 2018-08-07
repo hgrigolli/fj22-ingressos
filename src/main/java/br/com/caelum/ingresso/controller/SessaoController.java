05d1dc41cdfadb270637c90efcb442411e8950d3
Henrique Grigolli <henrigrigo@gmail.com>
2018-08-07 15:38:45 -0300

    Criado Testes de Unidade

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -17,6 +17,7 @@ import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.form.SessaoForm;
+import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
 
 @Controller
 public class SessaoController {
@@ -51,9 +52,16 @@ public class SessaoController {
 		
 		Sessao sessao = form.toSessao(salaDao, filmeDao);
 		
-		sessaoDao.save(sessao);
+		List<Sessao> sessoesDaSala = sessaoDao.buscaSessoesDaSala(sessao.getSala());
 		
-		return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoesDaSala);
+		
+		if(gerenciador.cabe(sessao)) {
+			sessaoDao.save(sessao);
+			return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+		}
+		
+		return form(form.getSalaId(), form);
 	}
 	
 }
---
+++ src/main/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessao.java
@@ -0,0 +1,29 @@
+package br.com.caelum.ingresso.validacao;
+
+import java.util.List;
+
+import br.com.caelum.ingresso.model.Sessao;
+
+public class GerenciadorDeSessao {
+	
+	private List<Sessao> sessoesDaSala;
+	
+	public GerenciadorDeSessao(List<Sessao> sessoesDaSala) {
+		this.sessoesDaSala = sessoesDaSala;
+	}
+
+	private static boolean isConflitante(Sessao sessaoNova, Sessao sessaoExistente) {
+		boolean novaTerminaAntesDaExistente = sessaoNova.getHorarioTermino().isBefore(sessaoExistente.getHorario());
+		boolean novaComecaDepoisDaExistente = sessaoNova.getHorario().isAfter(sessaoExistente.getHorarioTermino());
+		
+		if(novaTerminaAntesDaExistente || novaComecaDepoisDaExistente) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public boolean cabe(Sessao sessaoNova) {
+		return sessoesDaSala.stream().noneMatch(sessaoExistente -> isConflitante(sessaoNova, sessaoExistente));
+	}
+}
